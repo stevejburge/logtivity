4a8eb1dc64909a44e26aea7350c68267ea6462ab
Ralph Morris <ralph@10degrees.uk>
2021-07-10 23:03:50 +0100

    Track File download

--- Logs/Easy_Digital_Downloads/Logtivity_Easy_Digital_Downloads.php
+++ Logs/Easy_Digital_Downloads/Logtivity_Easy_Digital_Downloads.php
@@ -8,6 +8,7 @@ class Logtivity_Easy_Digital_Downloads extends Logtivity_Abstract_Logger
 		add_action('edd_post_remove_from_cart',  [$this, 'itemRemovedFromCart'], 10, 3);
 		add_action('edd_customer_post_create', [$this, 'customerCreated'], 10, 2);
 		add_action('edd_update_payment_status', [$this, 'paymentStatusUpdated'], 10, 3);
+		add_action('edd_process_verified_download', [$this, 'fileDownloaded'], 10, 4);
 	}
 
 	public function itemAddedToCart($download_id, $options, $items)
@@ -86,7 +87,6 @@ class Logtivity_Easy_Digital_Downloads extends Logtivity_Abstract_Logger
 			->addMeta('Currency', $payment->currency)
 			->addMeta('Gateway', $payment->gateway)
 			->addMeta('Customer ID', $customer->id)
-			->addMeta('Customer Email', $customer->email)
 			->send();
 	}
 
@@ -119,6 +119,33 @@ class Logtivity_Easy_Digital_Downloads extends Logtivity_Abstract_Logger
 			->setAction('Customer Created')
 			->setContext($args['name'])
 			->addMeta('Customer ID', $created)
+			->send();
+	}
+
+	public function fileDownloaded($download, $email, $payment, $args)
+	{
+		$download = new EDD_Download($download);
+
+		$payment = new EDD_Payment($payment);
+
+		$customer = new EDD_Customer($payment->get_meta( '_edd_payment_customer_id', true ));
+
+		$log = Logtivity_Logger::log()
+			->setAction('File Downloaded')
+			->setContext($download->post_title)
+			->addMeta('Payment Key', $this->getPaymentKey($payment));
+
+		if (isset($args['price_id'])) {
+			$prices = edd_get_variable_prices($download->get_ID());
+
+			if ($prices && count($prices)) {
+				if (isset($prices[$args['price_id']]['name'])) {
+					$log->addMeta('Variable Item', $prices[$args['price_id']]['name']);
+				}
+			}
+		}
+
+		$log->addMeta('Customer ID', $customer->id)
 			->send();
 	}
 }
