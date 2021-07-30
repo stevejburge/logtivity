890d497fed2ad769cb4ee7cacaf0a96eae80d86d
Ralph Morris <ralph@10degrees.uk>
2021-07-30 23:50:10 +0100

    Add tracking of deactivating licenses

--- Logs/Easy_Digital_Downloads/Logtivity_Easy_Digial_Downloads_Software_Licensing.php
+++ Logs/Easy_Digital_Downloads/Logtivity_Easy_Digial_Downloads_Software_Licensing.php
@@ -2,10 +2,15 @@
 
 class Logtivity_Easy_Digial_Downloads_Software_Licensing
 {
+	protected $deactivatingLicenseArgs;
+
 	public function __construct()
 	{
-		add_action( 'edd_sl_store_license', [$this, 'licenseCreated'], 10, 4);
-		add_filter( 'edd_sl_post_activate_license_result', [$this, 'licenseActivated'], 10, 2);
+		add_action('edd_sl_store_license', [$this, 'licenseCreated'], 10, 4);
+		add_filter('edd_sl_post_activate_license_result', [$this, 'licenseActivated'], 10, 2);
+		add_filter('edd_sl_pre_deactivate_license_args', [$this, 'deactivatingLicense']);
+		add_action('edd_sl_deactivate_license', [$this, 'licenseDeactivated'], 10, 2);
+		 
 	}
 
 	public function licenseCreated($license_id, $purchased_download_id, $payment_id, $type)
@@ -36,7 +41,7 @@ class Logtivity_Easy_Digial_Downloads_Software_Licensing
 			$log->setAction('License Activated');
 
 			if (isset($args['url'])) {
-				$log->addMeta('URL', $args['url']);
+				$log->addMeta('Site', $args['url']);
 			}
 
 			if (isset($result['site_count'])) {
@@ -61,6 +66,35 @@ class Logtivity_Easy_Digial_Downloads_Software_Licensing
 			->send();
 
 		return $result;
+	}
+
+	public function deactivatingLicense($args)
+	{
+		$this->deactivatingLicenseArgs = $args;
+
+		return $args;
+	}
+
+	public function licenseDeactivated($license_id, $download_id)
+	{
+		$license = edd_software_licensing()->get_license($this->deactivatingLicenseArgs['key'], true);
+
+		if ( empty( $this->deactivatingLicenseArgs['url'] ) && ! edd_software_licensing()->force_increase() ) {
+
+			// Attempt to grab the URL from the user agent if no URL is specified
+			$domain = array_map( 'trim', explode( ';', $_SERVER['HTTP_USER_AGENT'] ) );
+
+			if ( ! empty( $domain[1] ) ) {
+				$this->deactivatingLicenseArgs['url'] = trim( $domain[1] );
+			}
+		}
+
+		Logtivity_Logger::log()
+			->setAction('License Deactivated')
+			->setContext($this->deactivatingLicenseArgs['key'])
+			->setUser($license->user_id ?? null)
+			->addMeta('Site', $this->deactivatingLicenseArgs['url'] ?? $domain ?? null)
+			->send();
 	}
 }
 
