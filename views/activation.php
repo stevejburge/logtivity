ea7a3b5a01d07b6e6dfa21da55c4872f8f0eaf96
Steve Burge <steve@rambleventures.com>
2021-08-10 15:45:57 -0400

    Update activation.php
    small text update

--- views/activation.php
+++ views/activation.php
@@ -2,15 +2,14 @@
 
 	<img style="margin: 20px 0 20px; display: block; width: 200px; height: auto;" src="<?php echo plugin_dir_url('logtivity.php') ?>logtivity/assets/logtivity-logo.svg" alt="Logtivity">
 
-	<p><strong>Welcome to Logtivity!</strong> Here's how it works:</p>
+	<p><strong>Welcome to Logtivity!</strong> Take three simple steps to start your site's activity log:</p>
 
-	<ol>
-		<li>Make sure you have an account set up with <a target="_blank" href="https://logtivity.io/">Logtivity</a></li>
-		<li><a target="_blank" href="https://logtivity.io/docs/connect-your-site-to-logtivity/">Create a site</a> inside the Logtivity app and copy your API key.</li>
-		<li>Paste your API key in your <a href="<?php echo admin_url( 'tools.php?page=logtivity' ) ?>">plugin settings page</a>.</li>
-		<li>Configure your options and you're good to go!</li>
-	</ol>
+	<ul>
+		<li>Create an account with <a target="_blank" href="https://logtivity.io/">Logtivity</a>.</li>
+		<li>Copy your API key from Logtivity.</li>
+		<li>Paste your API key in this site's <a href="<?php echo admin_url( 'tools.php?page=logtivity' ) ?>">plugin settings page</a>.</li>
+	</ul>
 
-	<p>For more information getting started and what you can do with Logtivity <a href="http://logtivity.io/docs" target="_blank">read this guide here.</a></p>
+	<p>For more information on getting started and what you can do with Logtivity, <a href="http://logtivity.io/docs" target="_blank">read this guide here.</a></p>
 
 </div>
